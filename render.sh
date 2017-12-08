8a7899f37df1ed17cd8d0e3701d9060c4c1b6ffd
ThierryO <Thierry.Onkelinx@inbo.be>
2017-12-09 00:24:28 +0100

    check if rendering is required

--- render.sh
+++ render.sh
@@ -3,9 +3,7 @@ git config --global user.email "bmk@inbo.be"
 git config --global user.name "INBO CI"
 git clone --depth=1 --branch=gh-pages https://$GH_OAUTH@github.com/ThierryO/drat.git
 cd drat
-done=$(git log -1 | grep "Render markdown")
-echo $done
-if [[ "$done" = "Render markdown" ]]; then
+if [[ $(git log -1 | grep --quiet "Render markdown") ]]; then
   echo "Done"
 else
   Rscript "render.R"
