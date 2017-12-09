27062fcb32b693a63078cb07e45300a80716608c
ThierryO <Thierry.Onkelinx@inbo.be>
2017-12-09 12:07:33 +0100

    install missing dependencies manually

--- render.R
+++ render.R
@@ -13,16 +13,26 @@ tarbals <- sprintf(
 )
 junk <- lapply(tarbals, untar, exdir = tempdir())
 devtools::install_github("hadley/pkgdown")
+to.do <- as.vector(packages[, c("Suggests", "LinkingTo", "Depends", "Imports")])
+to.do <- paste(na.omit(to.do), collapse = ", ")
+to.do <- gsub("\\n", " ", to.do)
+to.do <- gsub(" \\(.*?\\)", "", to.do)
+dependencies <- unique(strsplit(to.do, ", ")[[1]])
+dependencies <- dependencies[dependencies != "R"]
+junk <- sapply(
+  dependencies,
+  function(x){
+    if (length(find.package(x, quiet = TRUE)) == 0) {
+      install.packages(x)
+    }
+  }
+)
 junk <- sapply(
   packages[, "Package"],
   function(package) {
     source <- paste(tempdir(), package, sep = "/")
     setwd(source)
     target <- sprintf("docs/%s", package)
-    devtools::install_local(
-      path = ".",
-      upgrade_dependencies = FALSE
-    )
     test <- try(pkgdown::build_site(preview = FALSE))
     if (inherits(test, "try-error")) {
       return(NULL)
