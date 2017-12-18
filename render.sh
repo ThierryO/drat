6b5da6961bda1fdffe1125c3ad84e4bae41bef36
ThierryO <Thierry.Onkelinx@inbo.be>
2017-12-18 10:40:06 +0100

    fix render script

--- render.sh
+++ render.sh
@@ -3,7 +3,7 @@ git config --global user.email "bmk@inbo.be"
 git config --global user.name "INBO CI"
 git clone --depth=1 --branch=gh-pages https://$GITHUB_PAT@github.com/ThierryO/drat.git
 cd drat
-if [[ $(git log -1 | grep --quiet "Render markdown") ]]; then
+if [[ $(git log -1 | grep "Render markdown") ]]; then
   echo "Done"
 else
   Rscript "render.R"
